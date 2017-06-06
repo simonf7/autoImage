2bf5afb11c3e3c75cb4a44c499f12c6d3db4ef5a
Simon Foster <me@simonfoster.co.uk>
2017-06-06 07:35:23 +0100

    Added commands to the script to stop caching and delete the cache for a specific image.

--- README.md
+++ README.md
@@ -7,15 +7,41 @@ In addition 400x400_test.jpg will be written to the folder to be returned next t
 
 **Note:** *transparency is not retained with PNG and GIF files*
 
+## Parameters
+The script can be passed a number of parameters which allow its behaviour to be controlled.
+
+Some of these require a passphrase to be passed to authenticate the request.
+
+### pp=<passphrase>
+This passphrase needs to match that set in the script.
+
+### cmd=<command>
+The command you wish to perform. Some are passed alongside the image request and others direct to the script.
+
+**nocache**
+Prevents the script from saving the cached copy of the new image on the server.
+
+*Example: /400x400_test.jpg?cmd=nocache*
+
+**delete**
+Call the script directly and delete copies of cached images. 
+
+An extra parameter **fn** needs to be passed to specify the name of the image.
+
+This command requies the passphrase to be passed and correct.
+
+*Example: /images.php?cmd=delete&fn=test.jpg&pp=secret_passphrase*
+
+## Configuration options
 At the head of the file there are a number of configurable options -
 
-## $allowedSizes
+### $allowedSizes
 An array containing a list of sizes that can be requested, specified as a string '400x400' etc.
 
 If this is null then any size can be requested.
 
-## $jpegQuality
+### $jpegQuality
 Affects the quality of saved JPEG files.
 
-## $passPhrase
-Pass phrase for accessing administion functions when implemented.
+### $passPhrase
+Pass phrase for accessing administion functions when implemented - this should be changed to something else.
--- images.php
+++ images.php
@@ -5,29 +5,59 @@
  * This file will allow an image to be automatically created based on the request
  */
 
-/** @var array $allowedSizes    This is a list of sizes that are allowed */
+
+/** @var array $allowedSizes        This is a list of sizes that are allowed */
 $allowedSizes = array('400x400', '290x180', '150x100');
 
-/** @var int jpegQuality        Default quality for JPEG images */
+/** @var int jpegQuality            Default quality for JPEG images */
 $jpegQuality = 85;
 
-/** @var string $passPhrase     Secret passphraes to allow admin functions */
-$passPhrase = 'justdoit';
+/** @var string $passPhrase         Secret passphrases to allow admin functions */
+$passPhrase = 'secret_passphrase'; /** Change this! */
 
 
-/**
- * Return a 404 file not found error back to the browser
- *
- * @param void
- *
- * @return void
- */
-function showNotFound()
-{
-    header('HTTP/1.0 404 Not Found', true, 404);
-    echo 'File not found';
-    die();
+/** @var boolean $boolAdm           Was the passphrase sent and correct, this is needed to run any commands */
+$boolPP = empty($_REQUEST['pp']) ? false : ($_REQUEST['pp'] == $passPhrase);
+
+/** @var string $reqCmd             Was there a command passed in the URL */
+$reqCmd = empty($_REQUEST['cmd']) ? null : $_REQUEST['cmd'];
+
+/** Delete any cached images */
+if ($reqCmd == 'delete' && $boolPP===true) {
+    /** @var string $reqFN          Was there a filename specified in the passed variables */
+    $reqFN = empty($_REQUEST['fn']) ? '' : $_REQUEST['fn'];
+    if (!$reqFN) {
+        die('Sorry, you must specify the file you want to delete.');
+    }
+    /** Go through each allowed size and remove the file is one already created */
+    foreach ($allowedSizes as $size) {
+        /** @var string $checkName  Create the filename to check for based on passed filename and size */
+        $checkName = __DIR__ . '/' . $size . '_' . $reqFN;
+        if (file_exists($checkName)) {
+            unlink($checkName);
+        }
+    }
+
+    /** Indicate that the command was successful */
+    echo 'OK';
+    exit(1);
 }
+
+
+    /**
+    * Return a 404 file not found error back to the browser
+    *
+    * @param void
+    *
+    * @return void
+    */
+    function showNotFound()
+    {
+        header('HTTP/1.0 404 Not Found', true, 404);
+        echo 'File not found';
+        die();
+    }
+
 
 /** @var string $requestUri     Work out what we're asking for */
 $requestUri = basename($_SERVER['REQUEST_URI']);
@@ -35,12 +65,12 @@ $requestUri = basename($_SERVER['REQUEST_URI']);
 /** @var string $fileName       Split off file name from the URI, discard the query string these will be in the $_REQUEST array */
 list($fileName, $queryString) = array_pad(explode('?', $requestUri, 2), 2, null);
 
-/** @var string $size           Split the file name into size */
-/** @var string $actualFile     and actual file name */
-list($size, $actualFile) = array_pad(explode('_', $fileName, 2), 2, null);
+/** @var string $reqSize            Split the file name into size */
+/** @var string $actualFile         and actual file name */
+list($reqSize, $actualFile) = array_pad(explode('_', $fileName, 2), 2, null);
 
 /** Make sure the size is listed in the $allowedSizes array */
-if ($allowedSizes!==null && !in_array($size, $allowedSizes)) {
+if ($allowedSizes!==null && !in_array($reqSize, $allowedSizes)) {
     showNotFound();
     die();
 }
@@ -52,16 +82,15 @@ if (!file_exists($actualFile)) {
     die();
 }
 
-/** @var int $newWidth          Dimensions of the new file requested, width
-/** @var int $newHeight         and height */
-list($newWidth, $newHeight) = explode('x', $size, 2);
+/** @var int $newWidth              Dimensions of the new file requested, width
+/** @var int $newHeight             and height */
+list($newWidth, $newHeight) = explode('x', $reqSize, 2);
 
-/** @var string $fileType       Get the file type */
+/** @var string $fileType           Get the file type */
 $fileType = strtolower(pathinfo($actualFile, PATHINFO_EXTENSION));
 
-/** @var object $srcImage       Work out the file type by using the file extension and create the image */
-switch ($fileType)
-{
+/** @var object $srcImage           Work out the file type by using the file extension and create the image */
+switch ($fileType) {
     case 'jpg':
     case 'jpeg':
         $srcImage = imagecreatefromjpeg($actualFile);
@@ -110,24 +139,39 @@ imagecopyresampled($tempImage, $srcImage, 0, 0, 0, 0, $tempWidth, $tempHeight, $
 $newImage = imagecreatetruecolor($newWidth, $newHeight);
 imagecopyresampled($newImage, $tempImage, 0, 0, (($tempWidth >> 1) - ($newWidth >> 1)), (($tempHeight >> 1) - ($newHeight >> 1)), $newWidth, $newHeight, $newWidth, $newHeight);
 
-/** Output the file */
-switch ($fileType)
-{
+
+/** Output the image to a file unless requested not to */
+if ($reqCmd != 'nocache') {
+    switch ($fileType) {
+        case 'jpg':
+        case 'jpeg':
+            imagejpeg($newImage, __DIR__ . '/' . $fileName, $jpegQuality);
+            break;        
+
+        case 'gif':
+            imagegif($newImage, __DIR__ . '/' . $fileName);
+            break;
+
+        case 'png':
+            imagepng($newImage, __DIR__ . '/' . $fileName);
+            break;
+    }
+}
+
+/** Send the image to the browser */
+switch ($fileType) {
     case 'jpg':
     case 'jpeg':
-        imagejpeg($newImage, __DIR__ . '/' . $fileName, $jpegQuality);
         header('Content-Type: image/jpeg');
         imagejpeg($newImage);
         break;
 
     case 'gif':
-        imagegif($newImage, __DIR__ . '/' . $fileName);
         header('Content-Type: image/gif');
         imagegif($newImage);
         break;
 
     case 'png':
-        imagepng($newImage, __DIR__ . '/' . $fileName);
         header('Content-Type: image/png');
         imagepng($newImage);
         break;
